8a965075fb2da1ad0fa077cc231104071897541d
iranika <7370486+iranika@users.noreply.github.com>
2022-09-27 13:15:40 +0900

    fix: 4komaDataの構造変更に合わせてpageの表示を修正

--- src/components/ViewerContent.vue
+++ src/components/ViewerContent.vue
@@ -153,17 +153,16 @@ export default defineComponent({
 
     function initShow(page: string){
       const pageInt = parseInt(page)
-      const ZureMin = 3
-      const ZureMax = 80
+      //const ZureMin = 3
+      //const ZureMax = 80
       if (page === 'latest'){
         return [pages.length -1]
-      }else if(ZureMin <= pageInt && pageInt <= ZureMax){
-        //NOTE: 3話が2話と同じページにあるためずれているが、話数にカウントされていないりれきしょのページでそのズレが解消されている奇跡的なバグがある。
-        return [Number(page) -2]
-      }else if (page === 'ri'){
-        return [79]
+      }
+      else if (page === 'ri'){
+        return [80]
       }else if (0 < pageInt && pageInt <= pages.length){
-        return [Number(page) -1]
+        const match = pages.filter(v => String(v.Index) == page)[0]
+        return [pages.indexOf(match)]
       }else if (pageInt <= 0){
         return [0]
       }else {
@@ -172,7 +171,7 @@ export default defineComponent({
       }
     }
     function CreateShareLinkTwitter(num: number, title: string) {
-      const tw_text = encodeURI(`第${num}話「${title}」`);
+      const tw_text = encodeURI(`第${num}話「${title}」`)
       return `https://twitter.com/intent/tweet?hashtags=%E9%81%93%E8%8D%89%E5%B1%8B&original_referer=https%3A%2F%2Fpublish.twitter.com%2F%3FbuttonText%3D%25E3%2581%2593%25E3%2581%2593%25E3%2581%2599%25E3%2581%258D%25EF%25BC%2581%25EF%25BC%2581%2523%25E3%2581%25BF%25E3%2581%25A1%25E3%2581%258F%25E3%2581%2595%25E3%2581%25B3%25E3%2582%2585%25E3%2581%2582%25E3%2583%25BC%26buttonType%3DTweetButton%26buttonUrl%3Dhttps%253A%252F%252Fmovue.iranika.info%252F%252310%26dnt%3D1%26lang%3Dja%26widget%3DButton&ref_src=twsrc%5Etfw&text=${tw_text}&tw_p=tweetbutton&url=https%3A%2F%2Fmovue.iranika.info%2F%23%2F%3Fpage%3D${num}`;
       //`<a class="tweetbtn hidden-image" href="https://twitter.com/intent/tweet?hashtags=%E9%81%93%E8%8D%89%E5%B1%8B&original_referer=https%3A%2F%2Fpublish.twitter.com%2F%3FbuttonText%3D%25E3%2581%2593%25E3%2581%2593%25E3%2581%2599%25E3%2581%258D%25EF%25BC%2581%25EF%25BC%2581%2523%25E3%2581%25BF%25E3%2581%25A1%25E3%2581%258F%25E3%2581%2595%25E3%2581%25B3%25E3%2582%2585%25E3%2581%2582%25E3%2583%25BC%26buttonType%3DTweetButton%26buttonUrl%3Dhttps%253A%252F%252Fmovue.iranika.info%252Fmo-code%252F%252310%26dnt%3D1%26lang%3Dja%26widget%3DButton&ref_src=twsrc%5Etfw&text=${tw_text}&tw_p=tweetbutton&url=https%3A%2F%2Fmovue.iranika.info%2Fmo-code%2F%23${pageNum +1}"><span class="label">ツイートする</span></a>`
     }
