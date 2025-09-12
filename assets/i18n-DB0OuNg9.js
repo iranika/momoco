f376d1a784d65f454978d155af59d332052db1fd
iranika <7370486+iranika@users.noreply.github.com>
2025-09-12 23:17:30 +0900

    Updates

--- assets/i18n-DB0OuNg9.js
+++ assets/i18n-DB0OuNg9.js
@@ -1,4 +1,4 @@
-import{e as yn,r as Sn,s as On,c as Le,w as tt,i as kn,d as ft,h as qt,F as Zt,g as zt,a as Pn,o as An,b as vn,f as Rn,T as Dn,j as Fn}from"./index-DV17vnkl.js";/*!
+import{e as yn,r as Sn,s as On,c as Le,w as tt,i as kn,d as ft,h as qt,F as Zt,g as zt,a as Pn,o as An,b as vn,f as Rn,T as Dn,j as Fn}from"./index-DNGatLw7.js";/*!
   * shared v11.1.12
   * (c) 2025 kazuya kawaguchi
   * Released under the MIT License.
