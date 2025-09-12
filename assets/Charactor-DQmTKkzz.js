ca89a21c9f3ab37896c5fc23910ef0c11b834106
iranika <7370486+iranika@users.noreply.github.com>
2025-09-12 22:51:42 +0900

    Updates

---
+++ assets/Charactor-DQmTKkzz.js
@@ -0,0 +1,42 @@
+import{Q as y,a as o}from"./QCard-CeaIiXNF.js";import{d as k,c as A,$ as L,a0 as h,a1 as n,a2 as e,f as t,a3 as i,a4 as p,aa as c,F as d,ab as m,a9 as g,af as v,ae as C,ac as x}from"./index-DV17vnkl.js";import{Q as Y}from"./QExpansionItem-C5JMjuDg.js";import{Q as P}from"./QPage-BcBpzhq5.js";import{u as _}from"./ProductStore-AvIu_Y0G.js";import"./use-dark-BlYQhwla.js";import"./use-id-CmcF3z4n.js";import"./QSeparator-DfSnLh0E.js";import"./use-model-toggle-Bz5vO_fB.js";import"./index-C0RmN517.js";const S=[{name:"seri",namae:"せり/せりさん",cv:"雁庵うずめ",cvArray:["雁庵うずめ"],icon:"/img/charactor/seri-205.jpg",text:`道草屋の女将。
+お母さんのような存在。お茶目で乙女。
+おしゃべり好きでトークスキルが高く、怪談話が得意。
+お酒好きで、酔うと絡んでくる。
+耳飾りは着脱式。住み込みのしろさん・ずなちゃんと一緒に道草屋で暮らしている。
+芹は春の七草のひとつ。`,contents:["せりさんぷる",'<iframe width="100%" height="315" src="https://www.youtube.com/embed/videoseries?list=PLSY2NdJVC2ydtFT3cSDbvJX75Fmgpb3K6" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>']},{name:"suzusiro",namae:"すずしろ/しろさん",icon:"/img/charactor/suzusiro-241.jpg",cv:'餅よもぎ→<a href="https://twitter.com/hiyori_voice">御崎ひより</a>→三月',cvArray:["餅よもぎ","御崎ひより","三月"],text:`せりさんの世話係。
+三番目に道草屋歴が長い。道草屋の働き者。
+せりさん、ずなちゃんと一緒に暮らしている。
+湿気で癖毛の髪がもじゃるのが悩み。梅雨は大敵。
+花粉も大敵。あとお腹のお肉も大敵…
+春の七草のすずしろは、大根の別名。`,contents:["しろさんぷる",'<iframe width="100%" height="315" src="https://www.youtube.com/embed/videoseries?list=PLSY2NdJVC2yeRJUxBt4Ea-pXXL8L8TUQZ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>']},{name:"suzuna",namae:"すずな/ずなちゃん",cv:'<a href="https://twitter.com/toudourenge">藤堂れんげ</a>',cvArray:["藤堂れんげ"],icon:"/img/charactor/suzuna-177.jpg",text:`道草屋の可愛い代表。私服がちょっとダサ可愛いのも魅力のひとつ。
+真面目な良い子で沢山の人から愛されている。
+出身は北海道。祖母の伝手で道草屋で働きはじめた。
+今はせりさん、しろさんと一緒に暮らしている。
+最年少ゆえ世間知らずな一面もあるが、田舎における生活スキルが高く無人販売所を使いこなす田舎暮らしのプロ。
+実は能力保持者で、霊感だけでなく優位性アピール無力化（ドヤ顔キャンセル）など稀有な能力を持っている可能性がある[要出典]。
+春の七草の菘(すずな)は、カブの別名`,contents:["ずなさんぷる",'<iframe width="100%" height="315" src="https://www.youtube.com/embed/videoseries?list=PLSY2NdJVC2yexGT79Qa4g0J450CKGuvtz" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>']},{name:"hakobera",namae:"はこべら/はこさん",icon:"/img/charactor/hako-256.jpg",cv:'藤村咲樹→<a href="https://twitter.com/kotoka_11_28">琴香</a>',cvArray:['藤村咲樹","琴香"'],text:`道草屋のエッチなお姉さん。
+せりさんと幼馴染で二番目に道草屋歴が長い。
+丁寧な言葉遣いとは裏腹に悪戯好き。ずなちゃんと旦那様がよく悪戯の被害を受けている。
+最近は、なっちゃんもよく被害にあっているとか。
+バイク乗りで愛車を褒めると心を開いてくれるらしい。
+はこべらは春の七草のひとつ。ナデシコ科ハコベ属（Stellaria）。`,contents:["はこさんぷる",'<iframe width="100%" height="315" src="https://www.youtube.com/embed/videoseries?list=PLSY2NdJVC2yedk4HUf0oUOPVuhfeXirnm" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>']},{name:"ine",namae:"いね/いねさん",cv:'<a href="https://twitter.com/hinyanm">真宮ひいろ</a>',cvArray:["真宮ひいろ"],icon:"/img/charactor/ine-177.jpg",text:`いつも眠そうでこちらまで眠くなる（癒し）
+たびさんとは同級生。
+めんどくさがりだけど根は真面目でしっかり働く。
+おせち大会で何度も優勝する強者だが料理は得意ではない。
+審査員の心をつかむテクニックが成せる技だ。
+実はツッコミが上手。サボテンとアロエ（名はルサンチマン）を育てている。
+イネ科イネ属の稲は現代の七草に数えられないが、七草粥にはお米として入っている。`,contents:["いねさんぷる",'<iframe width="100%" height="315" src="https://www.youtube.com/embed/videoseries?list=PLSY2NdJVC2ydScUTbTcXcQqzZwuQn88TG" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>']},{name:"tabirako",namae:"たびらこ/たびさん",cv:'<a href="https://twitter.com/aiedakyoko">愛枝今日子</a>→ルナ',cvArray:["愛枝今日子","ルナ"],icon:"/img/charactor/tabirako-185.jpg",text:`いねさんとは同級生。
+いねさんのお世話が大好きで、まるで保護者。
+溢れ出る母性のせいか奥様と間違われたことがある。
+道草屋と実家のうどん屋を掛け持ちしているワーカーホリックで、うどんへの信頼と情熱がすごい。
+鉄瓶を育てるのが上手。私服がちょっとダサ可愛いのも魅力の一つ。
+春の七草のほとけのざの別名がオニタビラコ。
+鬼のようなたびさんは見たことがない。`,contents:["たびさんぷる",'<iframe width="100%" height="315" src="https://www.youtube.com/embed/videoseries?list=PLSY2NdJVC2yc-mFTjtErv4DF9YO0rj665" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>']},{name:"inoko",namae:"いのこ/うりちゃん",cv:'<a href="https://twitter.com/hako_noah">箱河ノア</a>',cvArray:["箱河ノア"],icon:"/img/charactor/inoko-251.jpg",text:`名はいのこだが通称うりちゃん。ずなちゃんと仲良し。
+独特な敬語を使う。活発でずる賢いが、おかげで痛い目にもあっている。
+欲しいものを買うために道草屋で働き始め、一度退職したがとある理由で再就職したバイト戦士。
+はこさんから譲り受けたチョイノリ（原付）に乗っていたが、祖父の伝手でトリッカー（普通二輪）を譲り受けて乗り回している。
+夏の七草の猪子槌（いのこづち）に由来。`,contents:["いのこさんぷる",'<iframe width="100%" height="315" src="https://www.youtube.com/embed/videoseries?list=PLSY2NdJVC2yfRdhG6DHOxA-MG2Ziduz8j" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>']},{name:"natuna",namae:"なつな/なっちゃん",icon:"/img/charactor/natuna-250.jpg",cv:'<a target="_blank" href="https://twitter.com/tanba_usa">丹羽うさぎ</a>',cvArray:["丹羽うさぎ"],text:`道草屋のお客さん。苗字は橘。元社畜。
+いねさん・たびさんと同級生で、小学三年生に転校してから約15年ぶり？に村に帰ってきた。
+生粋のカフェインジャンキー。好きな珈琲豆はそこにあるやつ（なんでもいい）。
+いねさんのおかげで道草屋の電話番ができるが、彼女は従業員ではなく客だということを忘れてはならない。
+春の七草のなずなは夏には枯れることから「夏無（なつな）」から変化した説がある[要出典]。`,contents:["なっちゃんさんぷる",'<iframe width="100%" height="315" src="https://www.youtube.com/embed/videoseries?list=PLSY2NdJVC2yd2ErdrZWRF2eKj4uoR500x" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>']}],V=k({props:{name:String},data(){return{db:_().db}},computed:{},setup(){const l=_().db;return{getProductsBanner:A(()=>function(w,b="mainAct"){return l.products.filter(u=>w.some(a=>b=="mainAct"?u.mainAct.includes(a):u.subAct.includes(a))).map(u=>u.banner)}),chara:S,db:l}}}),Q=["src"],j={class:"text-h6",style:{margin:"10px auto 10px auto"}},N=["innerHTML"],J=["textContent"],B=["innerHTML"],H=["innerHTML"],M=["innerHTML"];function z(l,r,w,b,T,u){return n(),h(P,{padding:""},{default:e(()=>[t(y,{class:"chara-card q-mx-auto"},{default:e(()=>[t(o,null,{default:e(()=>[...r[0]||(r[0]=[i("div",{class:"text-h5",style:{margin:"10px auto 10px auto"}},"登場人物の紹介",-1),p(" ※いらにかの主観で書かれています。 ",-1)])]),_:1}),t(o,null,{default:e(()=>[(n(!0),c(d,null,m(l.chara,(a,f)=>(n(),h(g,{round:"","fab-mini":"",padding:"xs",key:f,to:"/charactor/"+a.name},{default:e(()=>[t(v,null,{default:e(()=>[i("img",{style:{width:"100%"},src:a.icon},null,8,Q)]),_:2},1024)]),_:2},1032,["to"]))),128)),t(g,{round:"","fab-mini":"",padding:"xs",to:"/charactor/all"},{default:e(()=>[t(v,null,{default:e(()=>[...r[1]||(r[1]=[p("All",-1)])]),_:1})]),_:1})]),_:1}),t(o,null,{default:e(()=>[(n(!0),c(d,null,m(l.chara,(a,f)=>(n(),c("div",{key:f},[l.name==a.name||l.name=="all"?(n(),h(y,{key:0,flat:""},{default:e(()=>[t(o,null,{default:e(()=>[i("div",j,x(a.namae),1),r[2]||(r[2]=p(" CV:",-1)),i("label",{innerHTML:a.cv},null,8,N)]),_:2},1024),t(o,{class:"text-subtitle2",style:{"white-space":"pre-wrap"}},{default:e(()=>[i("span",{textContent:x(a.text)},null,8,J)]),_:2},1024),t(o,null,{default:e(()=>[t(Y,{class:"text-center",label:"関連作品",style:{"background-color":"#f7f7f7"}},{default:e(()=>[t(y,{class:"text-left"},{default:e(()=>[t(o,null,{default:e(()=>[...r[3]||(r[3]=[p(" クリックすると作品ページに飛べます ",-1)])]),_:1}),t(o,null,{default:e(()=>[(n(!0),c(d,null,m(a.contents,s=>(n(),c("div",{key:s,style:{"margin-bottom":"4px"}},[i("div",{innerHTML:s},null,8,B)]))),128))]),_:2},1024),t(o,null,{default:e(()=>[r[4]||(r[4]=i("div",{style:{"mergin-bottom":"10px"}},"メイン出演",-1)),(n(!0),c(d,null,m(l.getProductsBanner(a.cvArray,"mainAct"),s=>(n(),c("div",{key:s},[i("div",{innerHTML:s,class:"panel"},null,8,H)]))),128))]),_:2},1024),t(o,null,{default:e(()=>[r[5]||(r[5]=i("div",{style:{"mergin-bottom":"10px"}},"サブ出演",-1)),(n(!0),c(d,null,m(l.getProductsBanner(a.cvArray,"subAct"),s=>(n(),c("div",{key:s},[i("div",{innerHTML:s,class:"panel"},null,8,M)]))),128))]),_:2},1024)]),_:2},1024)]),_:2},1024)]),_:2},1024)]),_:2},1024)):C("",!0)]))),128))]),_:1})]),_:1})]),_:1})}const O=L(V,[["render",z]]);export{O as default};
