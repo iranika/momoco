620dd2a996a797f3533ec38741c8100226e7452a
iranika109 <7370486+iranika@users.noreply.github.com>
2026-05-26 01:43:07 +0900

    fix: sw.jsが古いキャッシュを返してしまうバグの修正

--- quasar.config.ts
+++ quasar.config.ts
@@ -162,6 +162,10 @@ export default defineConfig((ctx) => {
     // https://v2.quasar.dev/quasar-cli-vite/developing-pwa/configuring-pwa
     pwa: {
       workboxMode: 'GenerateSW', // 'GenerateSW' or 'InjectManifest'
+      workboxOptions: {
+        skipWaiting: true,
+        clientsClaim: true,
+      },
       // swFilename: 'sw.js',
       // manifestFilename: 'manifest.json',
       // extendManifestJson (json) {},
